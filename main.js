ed7ca87aa055d7dc0c5ef71ade8960f107d6f6ee
Marco Jaramillo <marco.jaramillocast@gmail.com>
2023-08-28 20:07:52 -0700

    2023

--- main.js
+++ main.js
@@ -1,103 +1,103 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const calculateButton = document.getElementById("calculate-button");
-    const clearButton = document.getElementById("clear-button");
-    const checkboxes = document.querySelectorAll("input[type=checkbox]");
-    const resultElement = document.getElementById("result");
-  
-    calculateButton.addEventListener("click", calcular);
-    clearButton.addEventListener("click", limpiar);
-  
-    function calcular(event) {
+  const calculateButton = document.getElementById("calculate-button");
+  const clearButton = document.getElementById("clear-button");
+  const checkboxes = document.querySelectorAll("input[type=checkbox]");
+  const resultElement = document.getElementById("result");
+
+  calculateButton.addEventListener("click", calcular);
+  clearButton.addEventListener("click", limpiar);
+
+  function calcular(event) {
       event.preventDefault();
-  
+
       let totalScore = 0;
-  
+
       checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-          totalScore += parseInt(checkbox.value);
-        }
+          if (checkbox.checked) {
+              totalScore += parseInt(checkbox.value);
+          }
       });
-  
+
       resultElement.textContent = `Resultado:  ${totalScore}%`;
-  
+
       if (totalScore < 70) {
-        resultElement.style.color = "red";
+          resultElement.style.color = "red";
       } else {
-        resultElement.style.color = "green";
+          resultElement.style.color = "green";
       }
-    }
-  
-    function limpiar() {
+  }
+
+  function limpiar() {
       checkboxes.forEach(checkbox => {
-        checkbox.checked = false;
+          checkbox.checked = false;
       });
-  
+
       resultElement.textContent = "Resultado: ";
       resultElement.style.color = "initial";
-    }
-  });
-  
-
-
- // Función para imprimir los resultados
- document.getElementById("print-button").addEventListener("click", function () {
-  const formData = collectFormData();
-  const resultText = formatFormData(formData);
-
-  // Obtén el contenido del elemento con id "result"
-  const resultContent = document.getElementById("result").textContent;
-
-  // Abre una ventana emergente para mostrar los resultados y el contenido de "result"
-  const printWindow = window.open("", "_blank", "width=600,height=400");
-  printWindow.document.write("<pre>" + resultText + "\n\n" + resultContent + "</pre>");
-  printWindow.document.close();
-});
-
-// Función para recopilar los valores de los campos
-function collectFormData() {
-  const formData = {};
-  const campos = document.querySelectorAll("#quiz-form input[type='checkbox'], #activo");
-  
-  campos.forEach(campos => {
-    if (campos.type === "checkbox" && campos.checked) {
-      formData[campos.name] = campos.value;
-    } else if (campos.type === "text") {
-      formData[campos.name] = campos.value;
-    }
-  });
-
-  return formData;
-}
-
-// Función para formatear los resultados
-function formatFormData(formData) {
-  let resultText = "Resultado:\n\n";
-  for (const field in formData) {
-    resultText += `${field}: ${formData[field]}%\n`;
   }
-  return resultText;
-}
 
-// fecha y hora
-function mostrarFechaHora() {
-  const fechaHoraElement = document.getElementById("fecha");
+  // Función para imprimir los resultados
+  document.getElementById("print-button").addEventListener("click", function () {
+      const formData = collectFormData();
+      const resultText = formatFormData(formData);
 
-  const options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      hour12: false,
-      timeZoneName: "short"
-  };
+      // Agregar la fecha y hora al contenido
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleString();
+      const contentWithDate = `${resultText}\n\nFecha y hora de impresión: ${formattedDate}`;
 
-  setInterval(() => {
-      const fechaHoraActual = new Date();
-      const fechaHoraTexto = fechaHoraActual.toLocaleString("es-ES", options);
-      fechaHoraElement.textContent = fechaHoraTexto;
-  }, 1000); // Actualizar cada segundo (1000 ms)
-}
+      // Abre una ventana emergente para mostrar los resultados y el contenido de "result"
+      const printWindow = window.open("", "_blank", "width=600,height=400");
+      printWindow.document.write("<pre>" + contentWithDate + "</pre>");
+      printWindow.document.close();
+  });
 
-mostrarFechaHora(); // Llamar a la función para iniciarla
+  // Función para recopilar los valores de los campos
+  function collectFormData() {
+      const formData = {};
+      const campos = document.querySelectorAll("#quiz-form input[type='checkbox'], #activo");
+
+      campos.forEach(campos => {
+          if (campos.type === "checkbox" && campos.checked) {
+              formData[campos.name] = campos.value;
+          } else if (campos.type === "text") {
+              formData[campos.name] = campos.value;
+          }
+      });
+
+      return formData;
+  }
+
+  // Función para formatear los resultados
+  function formatFormData(formData) {
+      let resultText = "Resultado:\n\n";
+      for (const field in formData) {
+          resultText += `${field}: ${formData[field]}%\n`;
+      }
+      return resultText;
+  }
+
+  // fecha y hora
+  function mostrarFechaHora() {
+      const fechaHoraElement = document.getElementById("fecha");
+
+      const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: false,
+          timeZoneName: "short"
+      };
+
+      setInterval(() => {
+          const fechaHoraActual = new Date();
+          const fechaHoraTexto = fechaHoraActual.toLocaleString("es-ES", options);
+          fechaHoraElement.textContent = fechaHoraTexto;
+      }, 1000); // Actualizar cada segundo (1000 ms)
+  }
+
+  mostrarFechaHora(); // Llamar a la función para iniciarla
+});
